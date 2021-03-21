755f8405af6496a9b0d57a3bd15eab41d12981b6
Gabriel Vega <gvega001@gmail.com>
2021-03-21 13:26:36 -0700

    Created Gradebook Class and interface.

---
+++ StudentInformationSystemClassLibrary/Gradebook.cs
@@ -0,0 +1,29 @@
+﻿namespace StudentInformationSystemClassLibrary
+{
+    public class Gradebook
+    {
+        public int Id { get; set; }
+
+        public string Comment { get; set; }
+
+        public string EndDateTime { get; set; }
+
+        public string StartDateTime { get; set; }
+
+        public int GradeBookNumber { get; set; }
+
+        public int LinkedGroup { get; set; }
+
+        public string Name { get; set; }
+
+        public int Period { get; set; }
+
+        public int SchoolCode { get; set; }
+
+        public string EducatorEmailAddress { get; set; }
+
+        public string EducatorName { get; set; }
+
+        public int EducatorNumber { get; set; }
+    }
+}
---
+++ StudentInformationSystemClassLibrary/IIGradeBook.cs
@@ -0,0 +1,30 @@
+﻿namespace StudentInformationSystemClassLibrary
+{
+    public interface IIGradeBook
+    {
+        int Id { get; set; }
+
+        string Comment { get; set; }
+
+        string EndDateTime { get; set; }
+
+        string StartDateTime { get; set; }
+
+        int GradeBookNumber { get; set; }
+
+        int LinkedGroup { get; set; }
+
+        string Name { get; set; }
+
+        int Period { get; set; }
+
+        int SchoolCode { get; set; }
+
+        string EducatorEmailAddress { get; set; }
+
+        string EducatorName { get; set; }
+
+        int EducatorNumber { get; set; }
+
+    }
+}
